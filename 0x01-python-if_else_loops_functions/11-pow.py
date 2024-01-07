23847b2708dfd1aacba1245776b513da58a9385d
mmanyuat <mmanyuat201@gmail.com>
2024-01-06 23:16:27 -0800

    power

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= (a)
+        return result
+    else:
+        result = 1.0
+        for _ in range(int(abs(b))):
+            result *= (a)
+        return result
