364bef9e6da2123ed8c016fdea95c0bae1f17a10
mmanyuat <mmanyuat201@gmail.com>
2024-01-23 12:08:05 -0800

    raise a nameError

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    raise NameError('C is fun')
+    try:
+        raise_exception_msg(message="")
+    except NameError as ne:
+        print(ne)
