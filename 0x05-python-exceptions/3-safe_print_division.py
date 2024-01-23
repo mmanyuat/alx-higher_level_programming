5d8120d9e25098d1e87493833da30e72114e3818
mmanyuat <mmanyuat201@gmail.com>
2024-01-23 06:39:15 -0800

    up_all

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,6 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
-        return None
     finally:
         print("{}".format(result))
-    return result
+        return result
