754369958cc7d3284e219331e9623722dc686037
mmanyuat <mmanyuat201@gamil.com>
2024-01-30 01:35:14 +0000

    all

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+No module use here
+"""
+
+
+class Rectangle:
+	"""
+	Class that represents Rectangle
+
+	Attributes: not yet define
+	Methods: not yet defined
+
+	"""
+	pass
