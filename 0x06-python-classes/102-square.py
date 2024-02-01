8b0227d3d040eca9b8893dfa2bb0f622b7896837
mmanyuat <mmanyuat201@gmail.com>
2024-02-01 13:05:55 -0800

    the square

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+No module being imported
+"""
+
+
+class Square:
+    """
+    A class that represents a square
+     Attributs:
+     __size
+     Methods:
+     size, area, __init__
+     """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be a number")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __It__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
