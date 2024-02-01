6ff20e98ad304c2ac8f6cd24708efab03a15f33a
mmanyuat <mmanyuat201@gmail.com>
2024-02-01 00:01:50 -0800

    up

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+NO modules imported
+"""
+
+
+class Rectangle:
+    """
+    A class that represents a rctangle
+
+    Attributes:
+    width, hights
+
+    Methods:
+    width, heights, area, paremeter, area
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise TypeError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise TypeError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return(self.width * self.height)
+
+    def perimeter(self):
+        if self.height or self.width == 0:
+            return 0
+        else:
+            return 2 * (self.width * self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            return (("#" * self.width + "\n") * self.height).strip()
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
