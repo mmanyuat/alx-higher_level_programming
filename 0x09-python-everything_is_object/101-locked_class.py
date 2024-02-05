fd2849170c42b08ca5660320c4b21f9dd6601e70
mmanyuat <mmanyuat201@gmail.com>
2024-02-04 20:56:31 -0800

    answer

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""""
+No imported modules
+"""
+
+
+class LockedClass:
+    """
+    A class that only creates a methods whose name is first name
+    """
+    __slots__ = ["first_name"]
