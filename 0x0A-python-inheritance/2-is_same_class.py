c2a070527c829a491d4b8c03382f547dcc20d410
mmanyuat <mmanyuat201@gmail.com>
2024-02-06 05:02:17 -0800

    this part

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Nothing imported
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    check if obj is exactly an instance of class
+    """
+
+    return type(obj) is a_class
