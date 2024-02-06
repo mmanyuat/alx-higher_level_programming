c6ffad36f0422621d138e7dfdd660b21864bd452
mmanyuat <mmanyuat201@gmail.com>
2024-02-06 05:16:25 -0800

    this

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Nothing imported
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function to check whether the obj belongs to class
+    """
+    return isinstance(obj, a_class)
