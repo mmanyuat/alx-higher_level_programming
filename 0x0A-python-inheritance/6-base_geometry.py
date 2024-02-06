8fbe7652cbf7814e0fd339e78e130a77cbf42962
mmanyuat <mmanyuat201@gmail.com>
2024-02-06 06:04:34 -0800

    base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Nothing imported
+"""
+
+
+class BaseGeometry:
+    """
+    A class that represents BaseGeometry
+
+    Attributes:
+     Nothing
+     Methods: area
+     """
+
+    def area(self):
+        raise Exception("area() is not implemented")
