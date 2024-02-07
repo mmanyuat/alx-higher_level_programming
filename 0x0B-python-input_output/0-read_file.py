e71a8c091201fee0477a228dfac21aca474e0b94
mmanyuat <mmanyuat201@gmail.com>
2024-02-07 06:52:14 -0800

    this one

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Nothing imported
+"""
+
+
+def read_file(filename=""):
+    """
+    function that opens a file
+    """
+
+    with open(filename, encoding='utf-8') as f:
+        f_contents = f.read()
+        print(f_contents, end=' ')
+        if __name__ == "__main__":
+            read_file("my_file_0.txt")
