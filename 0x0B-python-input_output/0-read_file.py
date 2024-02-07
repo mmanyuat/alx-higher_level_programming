6aacb4598e1c11c0fa78c1c1f9c10e4390151a90
mmanyuat <mmanyuat201@gmail.com>
2024-02-07 07:07:03 -0800

    final

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,6 +11,6 @@ def read_file(filename=""):
 
     with open(filename, encoding='UTF8') as f:
         f_contents = f.read()
-        print(f_contents, end=' ')
+        print(f_contents, end='')
         if __name__ == "__main__":
             read_file("my_file_0.txt")
