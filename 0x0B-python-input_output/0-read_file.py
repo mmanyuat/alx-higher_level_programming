ab0c00de4f8f1342c198ed45e9a00a39b518f76a
mmanyuat <mmanyuat201@gamil.com>
2024-02-07 14:33:44 +0000

    this

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,13 +2,14 @@
 """
 Nothing imported here
 """
-
-
 def read_file(filename=""):
 	"""
 	function that returns a file
 	"""
 
-	with open(filename, encoding = "UTF8") as file:
+	with open(filename, encoding = 'utf8') as file:
 		file_content = file.read()
-		print(file_content)
+		print(file_content, end=' ')
+		
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
