8efa5c0ebe0a71e9747a5e7fd4cc97b920e2a58c
mmanyuat <mmanyuat201@gmail.com>
2024-02-07 07:02:06 -0800

    ans

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,23 +10,8 @@ def read_file(filename=""):
     function that opens a file
     """
 
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, encoding='UTF8') as f:
         f_contents = f.read()
         print(f_contents, end=' ')
         if __name__ == "__main__":
             read_file("my_file_0.txt")
-=======
-Nothing imported here
-"""
-def read_file(filename=""):
-	"""
-	function that returns a file
-	"""
-
-	with open(filename, encoding = 'utf8') as file:
-		file_content = file.read()
-		print(file_content, end=' ')
-		
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
->>>>>>> ab0c00de4f8f1342c198ed45e9a00a39b518f76a
