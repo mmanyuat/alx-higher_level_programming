5ad54632fadb1b9da90216fc166e32a3e033327b
mmanyuat <mmanyuat201@gmail.com>
2024-02-09 06:45:13 -0800

    load python obj

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+json file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Load from json file to python object
+    """
+
+    with open(filename, 'r') as file:
+        obj = json.load(file)
