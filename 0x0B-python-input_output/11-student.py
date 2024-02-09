bc1de8f8519bb132cdc527489c22c168e1efb9d4
mmanyuat <mmanyuat201@gmail.com>
2024-02-09 08:37:44 -0800

    this one

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -23,6 +23,7 @@ class Student:
             return {
                     attr: getattr(self, attr)
                     for attr in attrs if hasattr(self, attr)}
+
     def reload_from_json(self, json):
         """Replaces all atttributes of the student instance"""
         for key, value in json.items():
