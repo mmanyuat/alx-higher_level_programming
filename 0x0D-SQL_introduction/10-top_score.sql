552a951ba7159df0c5f2b53ee9c12e0a491fe205
mmanyuat <mmanyuat201@gmail.com>
2024-03-12 10:28:16 -0700

    the_ans

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,5 @@
 -- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 -- display score and name based on score ascending order
-SELECT name, score FROM second_table ORDER BY score DESC;
+SELECT name, score
+FROM second_table
+ORDER BY score DESC;
