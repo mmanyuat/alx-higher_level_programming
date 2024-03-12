11a22906edc3a2f7f637cb11a5be5d359bd206b7
mmanyuat <mmanyuat201@gmail.com>
2024-03-12 10:11:40 -0700

    sec_tab

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,10 @@
 -- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 -- command to create second table
-
 CREATE TABLE IF NOT EXISTS second_table(
 	id INT AUTO_INCREMENT,
 	name VARCHAR(256),
 	score INT);
+-- inserting values to second table
 INSERT INTO second_table (id,name, score)
 VALUES 
 (1,'John', 10),
