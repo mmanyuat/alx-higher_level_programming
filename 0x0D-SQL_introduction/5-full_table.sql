1d84ca66eb80392ed200e1fd95f403c992c186b8
mmanyuat <mmanyuat201@gmail.com>
2024-03-12 09:07:58 -0700

    second set

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
--- command to show the table fully 
+-- uses the database
+USE hbtn_0c_0;
+-- show creates
 SHOW CREATE first_table;
