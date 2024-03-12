1c2d3688f8b1aaf58a41b9106c32b581abaad728
mmanyuat <mmanyuat201@gmail.com>
2024-03-12 09:19:56 -0700

    ans

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,7 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 -- uses the database
 SELECT TABLE_NAME, CREATE TABLE
+-- the second part of the command
 FROM information_schema.tables
+-- where part
 WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
