10e8e4f1f00521ab055399e65a19979c845340de
mmanyuat <mmanyuat201@gmail.com>
2024-03-13 02:13:59 -0700

    taask1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Write a script that creates the MySQL server user user_0d_1
 -- creating a user
 CREATE USER IF NOT EXISTS 'user_0d_1'
-IDENTIFIED AS 'user_0d_1_pwd';
+IDENTIFIED BY 'user_0d_1_pwd';
 -- GRANTING PREMISSION
 GRANT ALL PRIVILEGES
 ON *.*
