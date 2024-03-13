efd2f2f1cb807f0108c659f1be3c8bbbee329387
mmanyuat <mmanyuat201@gmail.com>
2024-03-13 02:10:30 -0700

    the data_base

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- Write a script that creates the MySQL server user user_0d_1
 -- creating a user
-CREATE USER 'user_0d_1'
+CREATE USER IF NOT EXISTS 'user_0d_1'
 IDENTIFIED AS 'user_0d_1_pwd';
 -- GRANTING PREMISSION
-GRANT PRIVILEGES *
+GRANT ALL PRIVILEGES
 ON *.*
-TO 'user_0d_1'@'user_0d_1_pwd';
+TO 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2
+-- create databse
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED AS 'user_0d_2_pwd';
 -- GRANITING PERMISSION
