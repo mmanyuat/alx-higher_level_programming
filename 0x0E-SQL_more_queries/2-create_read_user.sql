b60983c6be20dc4983002d7688b42905fa6c799a
mmanyuat <mmanyuat201@gmail.com>
2024-03-13 19:58:37 -0700

    taks_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2
 -- create databse
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED AS 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_od_2;
 -- GRANITING PERMISSION
 GRANT SELECT
 ON hbtn_0d_2.*
