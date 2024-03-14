19ee21dfd332d64858ff2d9b4eed474b471c8743
mmanyuat <mmanyuat201@gmail.com>
2024-03-14 00:03:11 -0700

    identified_by

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 -- create databse
 CREATE DATABASE IF NOT EXISTS hbtn_od_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED AS 'user_0d_2_pwd';
+IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
 ON hbtn_0d_2.*
 TO 'user_0d_2'@'localhost' WITH GRANT  OPTION;
