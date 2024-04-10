fc7afa2bb9e79fbf25e3f1c3cb5f5ec094733a9f
mmanyuat <mmanyuat201@gmail.com>
2024-04-10 01:17:58 -0700

    task0

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+let firstline = 'C is fun';
+let secondline = 'Python is cool';
+let thirdline = 'JavaScript is amazing';
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
