4d8f1e70f53e1771a970731b7b8e9103c8500462
mmanyuat <mmanyuat201@gmail.com>
2024-06-10 02:30:11 -0700

    renew

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    idx = sentence[0] if length >= 0 else none
+    idx = sentence[0] if length >  0 else None
     return (length, idx)
