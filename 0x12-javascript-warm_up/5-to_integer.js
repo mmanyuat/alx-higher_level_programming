1ec317dbd953b4102a3bbe76d66d0eb8191b1ff0
mmanyuat <mmanyuat201@gmail.com>
2024-06-18 02:51:01 -0700

    warmu js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+args = process.argv.slice(2)
+
+args.forEach(arg => {
+	const num = Number(arg);
+	if (isNaN(num)){
+		console.log("Not a number");
+	} else {
+		console.log(`My number: ${num}`)
+	}
+});
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const args = process.argv;
+let x = args[2];
+let count = 0;
+while (count < x){
+	console.log(
+
