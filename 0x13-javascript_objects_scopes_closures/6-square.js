607614e3936be8a4afb1125e2cb9343dd8098170
mmanyuat <mmanyuat201@gmail.com>
2024-06-19 09:48:04 -0700

    the_end_task

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
