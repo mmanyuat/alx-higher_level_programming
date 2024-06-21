490af0d7733b1806ea71d92d5f782b2fb316651f
mmanyuat <mmanyuat201@gmail.com>
2024-06-21 02:02:30 -0700

    filter_db

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module connects to the mysql database and retrieve
+all states from state table using the id in ascending order
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Main entry point of the script, takes mysql
+    credentials and database name as arguments
+    """
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+        )
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
