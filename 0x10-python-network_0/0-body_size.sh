399b919b70bbba0b01674b2db0fa33f93f2c5cff
mmanyuat <mmanyuat201@gmail.com>
2024-07-04 14:09:39 -0700

    curl -get

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#check the url provided
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi 
-URL=$1
-curl -s -o /dev/null -w '%{size_download}\n' "$URL"
+#check the url provide
+curl -s "$1" | wc -c
