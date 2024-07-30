be289938d9b50e5cc214ee577d0f931071e8a0b1
mmanyuat <mmanyuat201@gmail.com>
2024-07-30 19:17:48 +0000

    write

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,7 +7,5 @@ const stringToWrite = process.argv[3];
 fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
   if (err) {
     console.error(err);
-    return;
   }
-  console.log('File written successfully');
 });
