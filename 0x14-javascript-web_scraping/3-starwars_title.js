b981c7c1b4abb395ab128508f214aab32f41703e
mmanyuat <mmanyuat201@gmail.com>
2024-07-30 20:30:20 +0000

    task3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Get the movie ID from the command-line arguments
+const movieId = process.argv[2];
+
+// Construct the API URL
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a GET request to the API
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  // Parse the response body as JSON
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
+//
