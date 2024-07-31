e2454812301e7762d6753e22422f42aeaa63eb26
mmanyuat <mmanyuat201@gmail.com>
2024-07-31 09:42:49 +0000

    task6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to retrieve data. Status code:', response.statusCode);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (!completedTasksByUser[todo.userId]) {
+        completedTasksByUser[todo.userId] = 0;
+      }
+      completedTasksByUser[todo.userId]++;
+    }
+  });
+
+  console.log(completedTasksByUser);
+});
