04a6669b2e31a89aef3ef0d9eedd0421d2f04078
mmanyuat <mmanyuat201@gmail.com>
2024-07-31 08:41:36 +0000

    task4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiurl = process.argv[2];
+const characterId = 18;
+request(apiurl, (error, response, body) => {
+  if (error) {
+    console.log('Error:', error);
+    return;
+  }
+  const films = JSON.parse(body);
+  let count = 0;
+  if (response.statusCode !== 200) {
+    console.error('Failed to retrieve data. Status code:', response.statusCode);
+    return;
+  }
+  films.forEach(film => {
+    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+      count++;
+    }
+  });
+  console.log(count);
+});
