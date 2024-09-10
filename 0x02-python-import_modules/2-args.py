8de549a8aa04b222cf50455c03b44020226e7540
mmanyuat <mmanyuat201@gmail.com>
2024-09-09 21:55:46 -0700

    cl-arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv)
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
 
-if n == 1:
-    print("0 arguments:")
-elif n == 2:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
-else:
-    print(n-1, "arguments:")
-    for i in range(1, n):
-        print("{} : {}".format(i, sys.argv[i]))
+    if n == 1:
+        print("0 arguments.")
+    elif n == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print(n-1, "arguments:")
+        for i in range(1, n):
+            print("{} : {}".format(i, sys.argv[i]))
