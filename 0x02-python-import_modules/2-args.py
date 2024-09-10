c083878e86f7cbfd644d897def34f583ebb65031
mmanyuat <mmanyuat201@gmail.com>
2024-09-09 22:03:21 -0700

    tupdated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     n = len(sys.argv) - 1
 
-    if n == 1:
+    if n == 0:
         print("0 arguments.")
-    elif n == 2:
+    elif n == 1:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
