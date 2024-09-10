9576f9b3167374fe4053057eb6607009ac2ce2df
mmanyuat <mmanyuat201@gmail.com>
2024-09-09 22:28:57 -0700

    answer

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# adding all the arguments passed
+
+if __name__ == "__main__":
+    import sys
+    total_sum = 0
+    for arg in sys.argv[1:]:
+        total_sum += int(arg)
+    print(total_sum)
