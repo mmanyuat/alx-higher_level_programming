0a20e89df13af8af64261b2959bca80291d685c1
mmanyuat <mmanyuat201@gmail.com>
2024-12-24 06:24:39 -0800

    more_list

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 
 def print_sorted_dictionary(a_dictionary):
     """Function to sort keys of a dictionary"""
-    for key in sorted(a_dictionary):
-        print(f"{key}: {a_dictionary[key]")
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
